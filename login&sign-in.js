a97a30e6171325d209be579571abd87425139d5d
nandana05-tech <145787651+nandana05-tech@users.noreply.github.com>
2024-07-09 10:46:22 +0700

    Update login&sign-in.js

--- login&sign-in.js
+++ login&sign-in.js
@@ -21,4 +21,20 @@
         dapat digunakan untuk memanipulasi daftar kelas.*/
       }, false)
     })
+    document.addEventListener('DOMContentLoaded', function () {/*document digunakan untuk titik masuk ke konten halaman web, 
+    function digunakan untuk Mencegah Pengiriman Data Tidak Valid*/
+    const akun = document.querySelectorAll('.list-group-item');/*const digunakan untuk deklarasi variabel
+    , document digunakan untuk titik masuk ke konten halaman web, yang merupakan pohon DOM, dan 
+    queryselectorall digunakan mendapatkan semua elemen yang ada pada class list-group item*/
+    akun.forEach(function(item) {/*forach untuk mengeksekusi sebanyak satu kali, item adalah 
+    nilai dari setiap elemen yang iterasi*/
+    item.addEventListener('click', function() {//menentukan tindakan eksekusi jika mengklik
+        const link = item.getAttribute('data-url');/*getAttribute digunakan untuk mendaatkan value*/
+            if (link) {
+                alert('Selamat datang di filmku')
+                window.location.href = link;
+                }
+            });
+        });
+    });
 })()
