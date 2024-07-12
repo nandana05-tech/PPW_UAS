4ce7dc71269ac0e2cd3b2ea6625704db8c27c961
nandana05-tech <145787651+nandana05-tech@users.noreply.github.com>
2024-07-12 21:22:06 +0700

    Update login&sign-in.js

--- login&sign-in.js
+++ login&sign-in.js
@@ -4,16 +4,16 @@
     const forms = document.querySelectorAll('.needs-validation') /*document digunakan untuk titik masuk ke konten halaman web, 
     queryselectorall digunakan mendapatkan semua elemen yang ada pada class needs-validation*/
     
-    Array.prototype.slice.call(forms) /*form adalah nilai dari setiap elemen yang iterasi*/
-    .forEach(function (form) { //forach untuk mengeksekusi sebanyak satu kali
+    Array.prototype.slice.call(forms) /*mengubah NodeList yang mirip array menjadi array sebenarnya.*/
+    .forEach(function (form) { //foreach untuk mengeksekusi sebanyak satu kali
       form.addEventListener('submit', function (event) { //menambahkan function untuk mempermudah validasi
         if (!form.checkValidity()) {//form.checkValidatiy = validasi semua elemen input di dalam sebuah formulir HTML
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault() // Mencegah form dari pengiriman dan refresh halaman
+          event.stopPropagation() //Menghentikan event agar tidak meneruskan capturing ke elemen-elemen lain.
         } else {
-            event.preventDefault()
-            alert('Selamat datang di filmku')
-            window.location.href = 'home.html'; 
+            event.preventDefault() // Mencegah form dari pengiriman dan refresh halaman
+            alert('Selamat datang di filmku')//sebagai peringatan jika berhasil login/sign.in
+            window.location.href = 'home.html'; //halaman yang akan dituju
           }
         
         form.classList.add('was-validated')/*classList adalah properti read-only yang 
@@ -21,20 +21,35 @@
         dapat digunakan untuk memanipulasi daftar kelas.*/
       }, false)
     })
-    document.addEventListener('DOMContentLoaded', function () {/*document digunakan untuk titik masuk ke konten halaman web, 
-    function digunakan untuk Mencegah Pengiriman Data Tidak Valid*/
     const akun = document.querySelectorAll('.list-group-item');/*const digunakan untuk deklarasi variabel
     , document digunakan untuk titik masuk ke konten halaman web, yang merupakan pohon DOM, dan 
     queryselectorall digunakan mendapatkan semua elemen yang ada pada class list-group item*/
-    akun.forEach(function(item) {/*forach untuk mengeksekusi sebanyak satu kali, item adalah 
-    nilai dari setiap elemen yang iterasi*/
-    item.addEventListener('click', function() {//menentukan tindakan eksekusi jika mengklik
-        const link = item.getAttribute('data-url');/*getAttribute digunakan untuk mendaatkan value*/
-            if (link) {
-                alert('Selamat datang di filmku')
-                window.location.href = link;
-                }
-            });
+    Array.prototype.slice.call(akun)/*mengubah NodeList yang mirip array menjadi array sebenarnya.*/
+    .forEach(function(item) {/*foreach untuk mengeksekusi sebanyak satu kali, item adalah 
+      nilai dari setiap elemen yang iterasi*/
+        item.addEventListener('click', function(click) {//menentukan tindakan eksekusi jika mengklik
+                alert('Selamat datang di filmku')//sebagai peringatan jika berhasil login
+                window.location.href = 'home.html';//halaman yang akan dituju
         });
     });
+    const forms2 = document.querySelectorAll('.needs-validation') /*document digunakan untuk titik masuk ke konten halaman web, 
+    queryselectorall digunakan mendapatkan semua elemen yang ada pada class needs-validation*/
+    
+    Array.prototype.slice.call(forms2) /*form adalah nilai dari setiap elemen yang iterasi*/
+    .forEach(function (form) { //forach untuk mengeksekusi sebanyak satu kali
+      form.addEventListener('submit', function (event) { //menambahkan function untuk mempermudah validasi
+        if (!form.checkValidity()) {//form.checkValidatiy = validasi semua elemen input di dalam sebuah formulir HTML
+          event.preventDefault() // Mencegah form dari pengiriman dan refresh halaman
+          event.stopPropagation() //Menghentikan event agar tidak meneruskan capturing ke elemen-elemen lain.
+        } else {
+            event.preventDefault() // Mencegah form dari pengiriman dan refresh halaman
+            alert('Terimakasih atas masukkannya')//sebagai peringatan jika berhasil memberi masukan
+            window.location.href = 'home.html'; //halaman yang akan dituju
+          }
+        
+        form.classList.add('was-validated')/*classList adalah properti read-only yang 
+        mengembalikan koleksi DOMTokenList langsung dari atribut kelas elemen. Ini kemudian 
+        dapat digunakan untuk memanipulasi daftar kelas.*/
+      }, false)
+    })
 })()
